2a4b253c8ad6893b7dc0ee27720056660e633605
DemeSaiKiran <demesaikiran84@gmail.com>
2020-04-27 21:08:17 +0530

    Sounds and Singleplayer commit

--- app/src/main/java/com/example/pigdice/SinglePlayer.java
+++ app/src/main/java/com/example/pigdice/SinglePlayer.java
@@ -2,9 +2,12 @@ package com.example.pigdice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class SinglePlayer extends AppCompatActivity {
+
+    private MediaPlayer rollsound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
