8af29d30244f0dcf5909c4736778deb17649760a
DemeSaiKiran <demesaikiran84@gmail.com>
2020-04-28 08:51:15 +0530

    Sounds and Singleplayer commit

--- app/src/main/java/com/example/pigdice/SinglePlayer.java
+++ app/src/main/java/com/example/pigdice/SinglePlayer.java
@@ -1,17 +1,84 @@
 package com.example.pigdice;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class SinglePlayer extends AppCompatActivity {
 
-    private MediaPlayer rollsound;
+    final Handler handler =new Handler();
+    private List<Integer> dieImages = new ArrayList();
+    private MediaPlayer rollSound;
+    private MediaPlayer shakeSound;
+    private Animation shake;
+    private Animation violentShake;
+    private TextView turnScore;
+    private TextView playerScoreText;
+    private int playerScore;
+    private int HoldScore;
+
+
+    public Button rollDice;
+    public Button holdDice;
+    public ImageView diceImage;
+    public int diceSum;
+    public int playerSum;
+    public AlertDialog dialog;
+    private Random random =new Random();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.singleplayer);
+
     }
+
+    public void handlePlayerRoll() {
+        //this.diceSum =this.dice.rollDie();
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SinglePlayer.this);
+
+
+        builder.setMessage("Giving Up?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        SinglePlayer.super.onBackPressed();
+
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
 }
--- app/src/main/java/com/example/pigdice/VsComputer.java
+++ app/src/main/java/com/example/pigdice/VsComputer.java
@@ -1,8 +1,15 @@
 package com.example.pigdice;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class VsComputer extends AppCompatActivity {
 
@@ -10,5 +17,60 @@ public class VsComputer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vscomputer);
+    }
+
+
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(VsComputer.this);
+
+        View view  = LayoutInflater.from(VsComputer.this).inflate(R.layout.custom_alert, null);
+        TextView title = (TextView) view.findViewById(R.id.title);
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.image);
+
+        title.setText("GivingUP?");
+
+        imageButton.setImageResource(R.drawable.gold);
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                VsComputer.super.onBackPressed();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.setView(view);
+        builder.show();
+
+        /*
+        builder.setMessage("Giving Up?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        VsComputer.super.onBackPressed();
+
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+         */
+
     }
 }
